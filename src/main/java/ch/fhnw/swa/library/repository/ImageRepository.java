d1adb7092bdadef4d0b394886c6afbe79f53cc41
Turan Ledermann <turan.ledermann@students.fhnw.ch>
2024-11-25 15:51:13 +0100

    add @Repository annotation

--- src/main/java/ch/fhnw/swa/library/repository/ImageRepository.java
+++ src/main/java/ch/fhnw/swa/library/repository/ImageRepository.java
@@ -2,8 +2,11 @@ package ch.fhnw.swa.library.repository;
 
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import ch.fhnw.swa.library.entity.Image;
 
+@Repository
 public interface ImageRepository extends MongoRepository<Image, ObjectId> {
 
 }
