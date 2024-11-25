0361dcb37faf775d75ed06bb387ed82384b3df13
Turan Ledermann <turan.ledermann@students.fhnw.ch>
2024-11-25 17:33:11 +0100

    save image reference in book objects

--- src/main/java/ch/fhnw/swa/library/controller/ImageController.java
+++ src/main/java/ch/fhnw/swa/library/controller/ImageController.java
@@ -5,12 +5,15 @@ import java.io.IOException;
 import org.bson.types.ObjectId;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import ch.fhnw.swa.library.entity.Book;
 import ch.fhnw.swa.library.entity.Image;
+import ch.fhnw.swa.library.service.BookService;
 import ch.fhnw.swa.library.service.ImageService;
 
 @RestController
@@ -19,14 +22,18 @@ public class ImageController{
 
 	private ImageService imageService;
 	
-	public ImageController(ImageService imageService) {
+	private BookService bookService;
+	
+	public ImageController(ImageService imageService, BookService bookService) {
 		this.imageService = imageService;
+		this.bookService = bookService;
 	}
 	
-	@GetMapping
-	public ObjectId addImage(@RequestParam("image") MultipartFile image) throws IOException {
-		ObjectId id = imageService.addImage(image);
-		return id;
+	@PostMapping
+	public ObjectId addImage(@RequestParam("image") MultipartFile image, String bookId) throws IOException {
+		Image newImage = imageService.addImage(image);
+		Book book = bookService.addBookImage(bookId, newImage);
+		return newImage.getId();
 	}
 	
 	@GetMapping("/{id}")
