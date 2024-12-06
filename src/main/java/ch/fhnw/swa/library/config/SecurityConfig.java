e0703484f89410f53e1ff4440f12b26c86e73555
Turan Ledermann <turan.ledermann@students.fhnw.ch>
2024-12-06 21:40:15 +0100

    use Spring Security User and UserDetailService

--- src/main/java/ch/fhnw/swa/library/config/SecurityConfig.java
+++ src/main/java/ch/fhnw/swa/library/config/SecurityConfig.java
@@ -1,32 +1,16 @@
 package ch.fhnw.swa.library.config;
 
-import java.math.BigInteger;
-import java.security.KeyFactory;
-import java.security.KeyPairGenerator;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.RSAPrivateKeySpec;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 
@@ -37,7 +21,11 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.authorizeHttpRequests(
-				(authorize) -> authorize.requestMatchers("/login").permitAll().anyRequest().authenticated());
+				(authorize) -> authorize
+				.requestMatchers("/login").permitAll()
+				.requestMatchers(HttpMethod.POST, "/images").hasRole("ADMIN")
+				.requestMatchers("/books").permitAll()
+				.requestMatchers("/users").permitAll());
 		return http.build();
 	}
 
@@ -49,17 +37,8 @@ public class SecurityConfig {
 		return new ProviderManager(authenticationProvider);
 	}
 	
-	@Bean UserDetailsService userDetailsService() {
-		UserDetails userDetails = User.withDefaultPasswordEncoder()
-				.username("test")
-				.password("test")
-				.roles("USER", "ADMIN")
-				.build();
-		
-		return new InMemoryUserDetailsManager(userDetails);
-	}
-	
-	@Bean PasswordEncoder passwordEncoder() {
-		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	@Bean 
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
--- src/main/java/ch/fhnw/swa/library/entity/User.java
+++ src/main/java/ch/fhnw/swa/library/entity/User.java
@@ -1,6 +1,7 @@
 package ch.fhnw.swa.library.entity;
 
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -11,20 +12,16 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
-public class User implements UserDetails, CredentialsContainer {
+public class User extends org.springframework.security.core.userdetails.User {
+
+	public User(String username, String password, Collection<? extends GrantedAuthority> authorities) {
+		super(username, password, authorities);
+	}
 
 	private static final long serialVersionUID = -7972231862009567627L;
 
 	@Id
 	private ObjectId id;
-
-	private String username;
-
-	private String password;
-
-	private Set<String> roles;
-
-	private boolean active;
 
 	public ObjectId getId() {
 		return id;
@@ -33,55 +30,4 @@ public class User implements UserDetails, CredentialsContainer {
 	public void setId(ObjectId id) {
 		this.id = id;
 	}
-
-	public String getUsername() {
-		return username;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
-	public void addRole(String role) {
-		roles.add(role);
-	}
-
-	public boolean removeRole(String role) {
-		return roles.remove(role);
-	}
-	
-	public Set<String> getRoles() {
-		return roles;
-	}
-
-	public void setRoles(Set<String> roles) {
-		this.roles = roles;
-	}
-
-	public boolean isActive() {
-		return active;
-	}
-
-	public void setActive(boolean active) {
-		this.active = active;
-	}
-
-	@Override
-	public void eraseCredentials() {
-		this.password = null;
-	}
-
-	@Override
-	public Collection<? extends GrantedAuthority> getAuthorities() {
-		return roles.stream().map(role -> new SimpleGrantedAuthority("ROLE_" + role)).collect(Collectors.toList());
-	}
-
 }
