c28be0f953cc78f103dccd7d622f61d7c2134b3b
Turan Ledermann <turan.ledermann@students.fhnw.ch>
2024-12-09 17:31:28 +0100

    add /users RequestMapping to UserController

--- src/main/java/ch/fhnw/swa/library/controller/UserController.java
+++ src/main/java/ch/fhnw/swa/library/controller/UserController.java
@@ -1,10 +1,12 @@
 package ch.fhnw.swa.library.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import ch.fhnw.swa.library.service.UserService;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
 
 	private UserService userService;
