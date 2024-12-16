e13f3d8ed0cffafca9831194282834199d319c14
Turan Ledermann <turan.ledermann@students.fhnw.ch>
2024-12-16 23:33:57 +0100

    add User validation and creation via POST

--- src/main/java/ch/fhnw/swa/library/controller/UserController.java
+++ src/main/java/ch/fhnw/swa/library/controller/UserController.java
@@ -1,8 +1,13 @@
 package ch.fhnw.swa.library.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import ch.fhnw.swa.library.entity.UserDTO;
 import ch.fhnw.swa.library.service.UserService;
 
 @RestController
@@ -13,5 +18,29 @@ public class UserController {
 	
 	public UserController(UserService userService) {
 		this.userService = userService;
+	}
+	
+	@PostMapping()
+	public ResponseEntity<Void> createUser(@RequestBody UserDTO user) {
+		if (isUserValid(user)) {
+			boolean userCreated = userService.createUser(user);
+			if (userCreated) {
+				return new ResponseEntity<>(HttpStatus.CREATED);
+			} else {
+				return new ResponseEntity<>(HttpStatus.FOUND);
+			}
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_ACCEPTABLE);
+		}
+	}
+	
+	private boolean isUserValid(UserDTO user) {
+		String username = user.username();
+		String password = user.password();
+		if (username.isBlank() || password.isBlank()) {
+			return false;
+		} else {
+			return true;
+		}
 	}
 }
