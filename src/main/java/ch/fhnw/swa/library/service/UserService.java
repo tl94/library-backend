9750cabc162e71150b7b8460a99636a4e71e566b
Turan Ledermann <turan.ledermann@students.fhnw.ch>
2024-12-18 16:18:52 +0100

    cleanup

--- src/main/java/ch/fhnw/swa/library/service/UserService.java
+++ src/main/java/ch/fhnw/swa/library/service/UserService.java
@@ -1,9 +1,5 @@
 package ch.fhnw.swa.library.service;
 
-import java.util.ArrayList;
-
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
