389d410ec7f1b579daa2ba7b0a47f5540d9e3479
Turan Ledermann <turan.ledermann@students.fhnw.ch>
2025-01-25 22:41:37 +0100

    implement image upload, remove old implementation

--- src/main/java/ch/fhnw/swa/library/controller/ImageController.java
+++ src/main/java/ch/fhnw/swa/library/controller/ImageController.java
@@ -1,8 +1,14 @@
 package ch.fhnw.swa.library.controller;
 
+import java.awt.Image;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.bson.types.ObjectId;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,33 +18,45 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import ch.fhnw.swa.library.entity.Book;
-import ch.fhnw.swa.library.entity.Image;
 import ch.fhnw.swa.library.service.BookService;
-import ch.fhnw.swa.library.service.ImageService;
 
 @RestController
 @RequestMapping("/images")
 public class ImageController{
-
-	private ImageService imageService;
+	
+	private final Environment environment;
+	
+	private static final String IMAGES_DIR = "images.dir";
 	
 	private BookService bookService;
 	
-	public ImageController(ImageService imageService, BookService bookService) {
-		this.imageService = imageService;
+	public ImageController(BookService bookService, Environment environment) {
 		this.bookService = bookService;
+		this.environment = environment;
 	}
 	
-	@PostMapping
-	public ObjectId addImage(@RequestParam MultipartFile image, String bookId) throws IOException {
-		Image newImage = imageService.addImage(image);
-		Book book = bookService.addBookImage(bookId, newImage);
-		return newImage.getId();
-	}
-	
-	@GetMapping("/{id}")
-	public Image getImage(@PathVariable ObjectId id) {
-		Image image = imageService.getImage(id);
-		return image;
+	@PostMapping()
+	public ResponseEntity<?> addImage(@RequestParam MultipartFile image, String bookId) throws IOException {
+		try {
+			Path uploadPath = Paths.get(environment.getProperty(IMAGES_DIR));
+			if (!Files.exists(uploadPath)) {
+				Files.createDirectories(uploadPath);
+			}
+			
+			Book book = bookService.findBookById(bookId);
+			
+			if (book != null) {
+				String fileName = book.getId() + "_" + (book.getImageRefs().size() + 1);
+				bookService.addBookImage(book.getId(), fileName);
+				Path filePath = uploadPath.resolve(fileName);
+				image.transferTo(filePath.toFile());
+				return ResponseEntity.ok().build();
+			} else {
+				return ResponseEntity.notFound().build();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+			return ResponseEntity.internalServerError().body("Failed to upload file.");
+		}
 	}
 }
